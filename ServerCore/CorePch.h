a58506e175c27f063eb590f1748d80c444e8a90e
jeondoh <jundo1302@gmail.com>
2025-03-19 23:25:54 +0900

    serverCore project init

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Types.h"
+
+#include <Windows.h>
+#include <iostream>
+
+#include <vector>
+#include <queue>
+#include <map>
+
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
