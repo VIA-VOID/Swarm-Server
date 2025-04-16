8bd06a87dd93bb417b33298808408ba7bb782f51
jeondoh <jundo1302@gmail.com>
2025-04-16 18:24:25 +0900

    feat: Lock, ThreadManager 매크로 추가

--- ServerCore/Pch/CoreMacro.h
+++ ServerCore/Pch/CoreMacro.h
@@ -1,21 +1,29 @@
 #pragma once
+#include "Log/LogDefine.h"
 #include <chrono>
 
 /*-------------------------------------
 	싱글톤 패턴 적용된 클래스들 정의
 -------------------------------------*/
 #define CLOCK					Clock::GetInstance()
-#define RANDOM					RandomMT::GetInstance()
-#define LOCK_M					LockDebugManger::GetInstance()
-#define LOG_M					LogManager::GetInstance()
+#define UTILS					Utils::GetInstance()
+#define LOCK					DeadlockDetector::GetInstance()
+#define LOG						LogDispatcher::GetInstance()
+#define THREAD_MANAGER			ThreadManager::GetInstance()
+
 
 /*-------------------------------------
 	함수 매크로
 -------------------------------------*/
 // 난수
-#define RAND(min, max)			RANDOM.GetRandom<decltype(min)>(min, max)
+#define RAND(min, max)			UTILS.GetRandom<decltype(min)>(min, max)
 // 현재시간
 #define NOW						std::chrono::steady_clock::now()
+// 로그
+#define LOG_INFO(msg)			LOG.PushLog(LogType::Info, msg)
+#define LOG_SYSTEM(msg)			LOG.PushLog(LogType::System, msg)
+#define LOG_ERROR(msg)			LOG.PushLog(LogType::Error, msg)
+
 
 /*-------------------------------------
 	커스텀 함수 매크로
@@ -38,17 +46,17 @@
 	}										\
 }
 
+
 /*-------------------------------------
 	커스텀 매크로
 -------------------------------------*/
 // debug 모드시 데드락 탐지
 #if _DEBUG
-#define USE_LOCK				Lock _lock
-#define LOCK_GURAD				LockGuard lockGuard(&_lock)
-#define UNIQUE_LOCK_GUARD		UniqueLockGuard ulockGuard(&_lock)
+#define USE_LOCK				Lock _mutex
+#define LOCK_GUARD				LockGuard lockGuard(&_mutex, typeid(this).name())
+#define UNIQUE_LOCK_GUARD		UniqueLockGuard ulockGuard(&_mutex, typeid(this).name())
 #else
 #define USE_LOCK				std::mutex _mutex
-#define LOCK_GURAD				std::lock_guard<std::mutex> lockGuard(_mutex)
+#define LOCK_GUARD				std::lock_guard<std::mutex> lockGuard(_mutex)
 #define UNIQUE_LOCK_GUARD		std::unique_lock<std::mutex> ulockGuard(_mutex)
 #endif
-
