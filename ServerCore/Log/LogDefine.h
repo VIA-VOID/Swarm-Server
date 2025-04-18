0b46adb5f7ab495886ad142ee7d3ca1eb40c6a87
jeondoh <jundo1302@gmail.com>
2025-04-18 19:16:10 +0900

    fix: WARNING 상태 추가

--- ServerCore/Log/LogDefine.h
+++ ServerCore/Log/LogDefine.h
@@ -9,6 +9,7 @@ enum class LogType : uint8
 {
 	Info,
 	System,
+	Warning,
 	Error,
 };
 
@@ -18,6 +19,7 @@ struct LogMessage
 	std::wstring _message;
 	std::wstring _timeStamp;
 	ThreadId _threadId;
+	const char* functionName;
 
 	std::wstring MakeLogWString()
 	{
@@ -27,17 +29,25 @@ struct LogMessage
 		switch (_type)
 		{
 		case LogType::Info:
-			ws << L" INFO   ";
+			ws << L" INFO    ";
 			break;
 		case LogType::System:
-			ws << L" SYSTEM ";
+			ws << L" SYSTEM  ";
+			break;
+		case LogType::Warning:
+			ws << L" WARNING ";
 			break;
 		case LogType::Error:
-			ws << L" ERROR  ";
+			ws << L" ERROR   ";
 			break;
 		}
 
-		ws << std::left << std::setw(6) << _threadId << L"--- " << _message << L"\n";
+		ws << std::left << std::setw(6)
+			<< _threadId << L"--- "
+			<< std::left << std::setw(30)
+			<< functionName
+			<< L" : "
+			<< _message << L"\n";
 		return ws.str();
 	}
 };
