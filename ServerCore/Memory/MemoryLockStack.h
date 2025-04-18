f2259ae5fa87f7978c016e8ee65a261ce39e9924
jeondoh <jundo1302@gmail.com>
2025-04-18 23:08:21 +0900

    feat: MemoryPool에서 사용하는 LockStack

---
+++ ServerCore/Memory/MemoryLockStack.h
@@ -0,0 +1,42 @@
+#pragma once
+
+/*----------------------------
+	MemoryLockStack
+
+- std::stack wrapping
+- MemoryPool에서 사용
+----------------------------*/
+template <typename T>
+class MemoryLockStack
+{
+public:
+	void Push(const T& item)
+	{
+		LOCK_GUARD;
+		_stack.push(item);
+	}
+
+	T Pop()
+	{
+		LOCK_GUARD;
+		T item = _stack.top();
+		_stack.pop();
+		return item;
+	}
+
+	T& Top()
+	{
+		LOCK_GUARD;
+		return _stack.top();
+	}
+
+	bool Empty()
+	{
+		LOCK_GUARD;
+		return _stack.empty();
+	}
+
+private:
+	USE_LOCK;
+	std::stack<T> _stack;
+};
