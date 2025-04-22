3db9ca478794644075c4449650b70c5f0bfde4f2
jeondoh <jundo1302@gmail.com>
2025-04-23 02:24:31 +0900

    feat: StealJob시 체크할 변수 추가

--- ServerCore/Pch/CoreTLS.cpp
+++ ServerCore/Pch/CoreTLS.cpp
@@ -3,3 +3,7 @@
 
 // 현재 소유하고 있는 LOCK
 thread_local std::stack<uint64> LHoldLock;
+// 스레드 이름
+thread_local std::string LThreadName;
+// StealJob시 마지막으로 훔친 JobGroupType 인덱스 번호
+thread_local uint16 LStealJobIndex;
--- ServerCore/Pch/CoreTLS.h
+++ ServerCore/Pch/CoreTLS.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <stack>
+#include <string>
 
 // 현재 소유하고 있는 LOCK
 extern thread_local std::stack<uint64> LHoldLock;
+// 스레드 이름
+extern thread_local std::string LThreadName;
+// StealJob시 마지막으로 훔친 JobGroupType 인덱스 번호
+extern thread_local uint16 LStealJobIndex;
