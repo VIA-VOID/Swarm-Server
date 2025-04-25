cc4f9620fcb5f10b7911cd247d88da2d7b850e4d
jeondoh <jundo1302@gmail.com>
2025-04-25 22:35:46 +0900

    feat: 네트워크 패킷 수신용

---
+++ ServerCore/Network/RecvBuffer.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "RecvBuffer.h"
+#include "Container/RingBuffer.h"
+
+/*----------------------------
+		RecvBuffer
+----------------------------*/
+
+RecvBuffer::RecvBuffer(uint32 bufferSize/* = BUFFER_SIZE*/)
+	: RingBuffer(bufferSize)
+{
+}
+
+// 데이터 읽기
+bool RecvBuffer::Read(BYTE* dest, uint32 size)
+{
+	if (size > GetUseSize())
+	{
+		return false;
+	}
+	// 데이터 빼오기
+	Peek(dest, size);
+	// readPos 이동
+	MoveReadPos(size);
+	return true;
+}
+
+// 패킷 크기 만큼의 연속 공간이 있는지 확인
+bool RecvBuffer::CanRecvPacketSize(uint32 packetSize)
+{
+	return GetUseSize() >= packetSize;
+}
+
+BYTE* RecvBuffer::GetRecvPtr()
+{
+	return GetWritePtr();
+}
+
+uint32 RecvBuffer::GetDirectRecvSize()
+{
+	return GetDirectDequeSize();
+}
---
+++ ServerCore/Network/RecvBuffer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Container/RingBuffer.h"
+
+/*-------------------------------------------------------
+				RecvBuffer
+--------------------------------------------------------*/
+
+class RecvBuffer : public RingBuffer
+{
+public:
+	RecvBuffer(uint32 bufferSize = BUFFER_SIZE);
+	virtual ~RecvBuffer() {};
+	// 데이터 읽기
+	bool Read(BYTE* dest, uint32 size);
+	// 패킷 크기 만큼의 연속 공간이 있는지 확인
+	bool CanRecvPacketSize(uint32 packetSize);
+	// getter -
+	BYTE* GetRecvPtr();
+	uint32 GetDirectRecvSize();
+};
