b4f988c6476aa62f1e65581abb80e310e119bf84
jeondoh <jundo1302@gmail.com>
2025-04-28 23:27:56 +0900

    feat: Array 추가, 문자열 pool 삭제

--- ServerCore/Container/Container.h
+++ ServerCore/Container/Container.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Memory/STLAllocator.h"
 
+#include <array>
 #include <vector>
 #include <list>
 #include <queue>
@@ -9,6 +10,9 @@
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
+
+template <typename Type, uint32 Size>
+using Array = std::array<Type, Size>;
 
 template <typename Type>
 using Vector = std::vector<Type, STLAllocator<Type>>;
@@ -33,12 +37,6 @@ using Stack = std::stack<Type, Container>;
 
 template <typename Type, typename Container = Vector<Type>, typename Pred = std::less<typename Container::value_type>>
 using PriorityQueue = std::priority_queue<Type, Container, Pred>;
-
-using String = std::basic_string<char, std::char_traits<char>, STLAllocator<char>>;
-
-using WString = std::basic_string<wchar_t, std::char_traits<wchar_t>, STLAllocator<wchar_t>>;
-
-using StringStream = std::basic_stringstream<wchar_t, std::char_traits<wchar_t>, STLAllocator<wchar_t>>;
 
 template <typename Key, typename Type, typename Hasher = std::hash<Key>, typename KeyEq = std::equal_to<Key>>
 using HashMap = std::unordered_map<Key, Type, Hasher, KeyEq, STLAllocator<std::pair<const Key, Type>>>;
