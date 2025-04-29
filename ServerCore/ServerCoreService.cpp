62a849d71287360a68c616f7a0b296958415d582
jeondoh <jundo1302@gmail.com>
2025-04-29 23:19:08 +0900

    feat: 외부에서 상속받아서 사용하는 Service

---
+++ ServerCore/ServerCoreService.cpp
@@ -0,0 +1,58 @@
+#include "pch.h"
+#include "ServerCoreService.h"
+
+
+/*----------------------------
+		NetworkService
+----------------------------*/
+
+NetworkService::NetworkService()
+{
+	MemoryMgr.Init();
+
+#if _DEBUG
+	::SetConsoleOutputCP(CP_UTF8);
+	DeadLockMgr.Init();
+#endif
+	ThreadMgr.Init();
+	JobQ.Init();
+	SessionMgr.Init();
+	LogMgr.Init();
+	CrashDump::Init();
+
+	LOG_SYSTEM(L"GlobalService instance initialized");
+}
+
+/*----------------------------
+		ServerCoreService
+----------------------------*/
+
+ServerCoreService::ServerCoreService()
+	: _iocpServer(nullptr)
+{
+}
+
+// iocpServer와 연결 및 시작 
+void ServerCoreService::StartServer(uint16 port)
+{
+	_iocpServer = ObjectPool<IocpServer>::Allocate();
+	_iocpServer->ConnectService(this);
+	_iocpServer->Start(port);
+}
+
+// 자원 해제
+void ServerCoreService::CloseServer()
+{
+	_iocpServer->Shutdown();
+	ObjectPool<IocpServer>::Release(_iocpServer);
+
+#if _DEBUG
+	DeadLockMgr.Shutdown();
+#endif
+
+	LogMgr.Shutdown();
+	JobQ.Shutdown();
+	SessionMgr.Shutdown();
+	ThreadMgr.Shutdown();
+	MemoryMgr.Shutdown();
+}
