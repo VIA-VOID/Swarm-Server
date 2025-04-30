5152ea66cb891cc5d8030c1abc1e76bbd5ab4b4d
jeondoh <jundo1302@gmail.com>
2025-05-01 03:50:06 +0900

    feat: JobGroup, JobGroupType 관리 매니저 클래스

---
+++ ServerCore/Job/JobGroupManager.cpp
@@ -0,0 +1,73 @@
+#include "pch.h"
+#include "JobGroupManager.h"
+
+void JobGroupManager::Init()
+{
+	_nextGroupId.store(JobGroups::NextStart, std::memory_order_relaxed);
+	// 기본적인 ServerCore 그룹 생성
+	RegisterGroup(JobGroups::System, "System", 1);
+	RegisterGroup(JobGroups::Network, "Network", 1, JobPriority::High);
+	RegisterGroup(JobGroups::Packet, "Packet", 4, JobPriority::High);
+	RegisterGroup(JobGroups::Log, "Log", 1, JobPriority::Low);
+}
+
+void JobGroupManager::Shutdown()
+{
+	// JobGroupType 반환
+	for (auto& group : _groups)
+	{
+		ObjectPool<JobGroupType>::Release(group.second);
+	}
+	_groups.clear();
+}
+
+// 컨텐츠 그룹 등록
+JobGroupId JobGroupManager::RegisterContentGroup(const std::string& name, uint16 threadCount, JobPriority priority /*= JobPriority::Normal*/, bool isInit /*= true*/)
+{
+	// 다음 사용 가능한 컨텐츠 그룹 ID 생성
+	JobGroupId newId = _nextGroupId.fetch_add(1);
+	return RegisterGroup(newId, name, threadCount, priority, isInit);
+}
+
+// ID로 그룹 정보 가져오기
+const JobGroupType* JobGroupManager::GetGroupInfo(JobGroupId id) const
+{
+	auto it = _groups.find(id);
+	if (it != _groups.end())
+	{
+		return it->second;
+	}
+	return nullptr;
+}
+
+// 모든 JobGroup 정보 가져오기
+const HashMap<JobGroupId, JobGroupType*>& JobGroupManager::GetAllGroups() const
+{
+	return _groups;
+}
+
+// ID에 해당하는 이름 가져오기
+const char* JobGroupManager::GetGroupName(JobGroupId id) const
+{
+	auto it = _groups.find(id);
+	if (it != _groups.end() && it->second != nullptr)
+	{
+		return it->second->GetGroupName().c_str();
+	}
+	return "System";
+}
+
+// 그룹 등록
+JobGroupId JobGroupManager::RegisterGroup(JobGroupId id, const std::string& name, uint16 threadCount, JobPriority priority /*= JobPriority::Normal*/, bool isInit /*= true*/)
+{
+	// 이미 등록된 ID인지 확인
+	if (_groups.find(id) != _groups.end())
+	{
+		LOG_WARNING(L"이미 등록된 JobGroup ID: " + std::to_wstring(id));
+		return JobGroups::Invalid;
+	}
+
+	JobGroupType* newGroup = ObjectPool<JobGroupType>::Allocate(id, name, threadCount, isInit, priority);
+	_groups[id] = newGroup;
+	return id;
+}
---
+++ ServerCore/Job/JobGroupManager.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <typeindex>
+
+/*-------------------------------------------------------
+				JobGroupManager
+
+- JobGroup 타입 관리
+--------------------------------------------------------*/
+
+class JobGroupManager : public Singleton<JobGroupManager>
+{
+public:
+	void Init() override;
+	void Shutdown() override;
+	// 컨텐츠 그룹 등록
+	JobGroupId RegisterContentGroup(const std::string& name, uint16 threadCount, JobPriority priority = JobPriority::Normal, bool isInit = true);
+	// ID로 그룹 정보 가져오기
+	const JobGroupType* GetGroupInfo(JobGroupId id) const;
+	// 모든 JobGroup 정보 가져오기
+	const HashMap<JobGroupId, JobGroupType*>& GetAllGroups() const;
+	// ID에 해당하는 이름 가져오기
+	const char* GetGroupName(JobGroupId id) const;
+	// 클래스 타입을 JobGroupId에 매핑
+	template <typename T>
+	void RegisterTypeMapping(JobGroupId groupId);
+	// 타입으로 GroupId 가져오기
+	template <typename T>
+	JobGroupId GetGroupIdByType() const;
+
+private:
+	// 그룹 등록
+	JobGroupId RegisterGroup(JobGroupId id, const std::string& name, uint16 threadCount, JobPriority priority = JobPriority::Normal, bool isInit = true);
+
+private:
+	std::atomic<JobGroupId> _nextGroupId;
+	HashMap<JobGroupId, JobGroupType*> _groups;
+	HashMap<std::type_index, JobGroupId> _typeToGroup;
+};
+
+// 클래스 타입을 JobGroupId에 매핑
+template<typename T>
+inline void JobGroupManager::RegisterTypeMapping(JobGroupId groupId)
+{
+	std::type_index typeIndex(typeid(T));
+	_typeToGroup[typeIndex] = groupId;
+}
+
+// 타입으로 GroupId 가져오기
+template<typename T>
+inline JobGroupId JobGroupManager::GetGroupIdByType() const
+{
+	std::type_index typeIndex(typeid(T));
+	auto it = _typeToGroup.find(typeIndex);
+	if (it != _typeToGroup.end())
+	{
+		return it->second;
+	}
+	// 기본값으로 시스템 그룹 반환
+	return JobGroups::System;
+}
+
+namespace std {
+	template<>
+	struct hash<JobGroupType>
+	{
+		uint64 operator()(const JobGroupType& group) const noexcept
+		{
+			return group.hash();
+		}
+	};
+}
