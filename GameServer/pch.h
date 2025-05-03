f3935a483bec488c50fcb6711d2b57be72eb6663
jeondoh <jundo1302@gmail.com>
2025-05-03 23:41:54 +0900

    feat: protobuf 라이브러리 추가

--- GameServer/pch.h
+++ GameServer/pch.h
@@ -2,6 +2,12 @@
 
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 
-#pragma comment(lib, "serverCore.lib")
+#ifdef _DEBUG
+#pragma comment(lib, "ServerCore\\x64Debug\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\x64Debug\\libprotobufd.lib")
+#else
+#pragma comment(lib, "ServerCore\\x64Release\\serverCore.lib")
+#pragma comment(lib, "Protobuf\\x64Release\\libprotobuf.lib")
+#endif
 
 #include "Pch/CorePch.h"
