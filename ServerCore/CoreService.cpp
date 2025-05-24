6720c2a621128e25794645646211b6c447ce0ca4
jeondoh <jundo1302@gmail.com>
2025-05-24 11:55:16 +0900

    feat: dummyClient 연결

--- ServerCore/CoreService.cpp
+++ ServerCore/CoreService.cpp
@@ -27,7 +27,7 @@ CoreService::CoreService(ServiceType type)
 	CrashDump::Init();
 }
 
-void CoreService::Start(uint16 port, const std::string& address /*= ""*/)
+void CoreService::Run(uint16 port, const std::string& address /*= ""*/, int16 connectCount /*= 1*/)
 {
 	_iocpCore = ObjectPool<IocpCore>::Allocate();
 	_iocpCore->ConnectService(this);
@@ -38,7 +38,7 @@ void CoreService::Start(uint16 port, const std::string& address /*= ""*/)
 	}
 	else
 	{
-		_iocpCore->Connect(address, port);
+		DummyConnect(port, address, connectCount);
 	}
 }
 
@@ -62,4 +62,28 @@ void CoreService::Stop()
 	JobGroupMgr.Shutdown();
 	ThreadMgr.Shutdown();
 	MemoryMgr.Shutdown();
+}
+
+void CoreService::DummyConnect(uint16 port, const std::string& address, int16 connectCount)
+{
+	// 워커 스레드 생성
+	_iocpCore->StartWorkerThreads(MAX_WORKER_THREAD_NUM);
+
+	LOG_SYSTEM(L"Dummy Client 생성 시작");
+	
+	// BATCH_SIZE 만큼 나누어서 연결
+	for (int32 i = 0; i < connectCount; i += BATCH_SIZE)
+	{
+		int32 batchCount = min(BATCH_SIZE, connectCount - i);
+
+		for (int32 j = 0; j < batchCount; j++)
+		{
+			_iocpCore->Connect(address, port);
+		}
+
+		LOG_SYSTEM(L"Connect: " + std::to_wstring(i) + L" ~ " + std::to_wstring(i + batchCount - 1));
+
+		// 배치 간 간격
+		std::this_thread::sleep_for(std::chrono::milliseconds(BATCH_DELAY_MS));
+	}
 }
--- ServerCore/CoreService.h
+++ ServerCore/CoreService.h
@@ -3,6 +3,12 @@
 class Session;
 class IocpCore;
 
+// dummyClient 생성시 필요
+// 배치 크기, BATCH_SIZE 크기만큼 나누어 연결
+constexpr uint32 BATCH_SIZE = 100;
+// 배치 간 딜레이
+constexpr uint32 BATCH_DELAY_MS = 100;
+
 enum ServiceType : uint16
 {
 	Client,
@@ -21,7 +27,7 @@ public:
 	CoreService(ServiceType type);
 	virtual ~CoreService() = default;
 
-	void Start(uint16 port, const std::string& address = "");
+	void Run(uint16 port, const std::string& address = "", int16 connectCount = 1);
 	void Stop();
 
 	// 컨텐츠 로직에서 구현
@@ -29,6 +35,9 @@ public:
 	virtual void OnDisconnected(Session* session) = 0;
 	virtual void OnRecv(Session* session, const BYTE* buffer, int32 len) = 0;
 	virtual void OnSend(Session* session, int32 len) = 0;
+
+private:
+	void DummyConnect(uint16 port, const std::string& address, int16 connectCount);
 
 private:
 	IocpCore* _iocpCore;
