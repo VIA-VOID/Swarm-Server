99a68fdd8c373adaf1e5cd5687af3ed4f39e92b3
jeondoh <jundo1302@gmail.com>
2025-06-16 11:25:07 +0900

    fix: CLASS_TO_JOB 제거, 추후 Zone 방식으로 그룹 변경

--- GameServer/Job/JobGroup.cpp
+++ GameServer/Job/JobGroup.cpp
@@ -1,26 +1,30 @@
 #include "pch.h"
 #include "JobGroup.h"
 #include "Character/CharacterObject.h"
+#include "Map/BaseMap.h"
 
 #include <typeindex>
 
 #define JOB_GROUP(name, priority) \
-    name = JobGroupMgr.RegisterContentGroup(#name, priority);
-
-#define CLASS_TO_JOB(className, groupName) \
-    JobGroupMgr.SetTypeToGroup(std::type_index(typeid(className)), groupName); \
-    JobQ.RegisterThreadsForGroup(groupName);
-
+    name = JobGroupMgr.RegisterContentGroup(#name, priority); \
+	JobQ.MakeGroupThread(JobGroup::name);
+	
 /*----------------------------
 		JobGroup
 ----------------------------*/
 
 // 정적 멤버 정의
-JobGroupId JobGroup::Player = UINT16_MAX;
+JobGroupId JobGroup::Character = JobGroups::Invalid;
+JobGroupId JobGroup::Enviroment = JobGroups::Invalid;
 
 // JobGroup 등록
 void JobGroup::Init()
 {
-	JOB_GROUP(JobGroup::Player, JobPriority::High);
-	CLASS_TO_JOB(CharacterObject, JobGroup::Player);
+	// Character 그룹 추가
+	// - Player 관련 모든 작업, 몬스터 전투 등
+	JOB_GROUP(Character, JobPriority::High);
+
+	// Enviroment 그룹 추가
+	// - 채팅, 맵 업데이트 등
+	JOB_GROUP(Enviroment, JobPriority::Normal);
 }
