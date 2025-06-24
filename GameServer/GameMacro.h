29dfa85e0fe993e94e947c9689c75d2ebd9f76ad
jeondoh <jundo1302@gmail.com>
2025-06-24 23:38:57 +0900

    feat: 매크로 파일 추가

---
+++ GameServer/GameMacro.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/*-------------------------------------
+	싱글톤 패턴 적용된 클래스들 정의
+-------------------------------------*/
+#define WorldMgr						WorldManager::GetInstance()
+
+
+
+/*-------------------------------------
+			스마트 포인터
+-------------------------------------*/
+SHARED_PTR(Player)
+SHARED_PTR(GameObject)
+
+UNIQUE_PTR(BaseZone)
