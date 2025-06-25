3f011c6580f3999bab0342fa1c07d487f9475b66
jeondoh <jundo1302@gmail.com>
2025-06-25 23:24:45 +0900

    feat: Player 패킷 정보 생성 추가

--- GameServer/Object/Player/Player.cpp
+++ GameServer/Object/Player/Player.cpp
@@ -1,13 +1,13 @@
 #include "pch.h"
 #include "Player.h"
-#include "Zone/MapDefine.h"
+#include "Object/Stat/StatManager.h"
 
 /*----------------------------
 		Player
 ----------------------------*/
 
 Player::Player(SessionRef session, const Protocol::PlayerType& playerType, const std::string& name)
-	: _session(session), _playerType(playerType), _currentMap(MapIds::Invalid)
+	: _session(session), _playerType(playerType)
 {
 	_objectType = Protocol::OBJECT_TYPE_PLAYER;
 	_name = name;
@@ -17,17 +17,17 @@ Player::~Player()
 {
 }
 
+// 초기 스텟 정보 초기화
+void Player::InitStatInfo()
+{
+	StatMgr.GetPlayerStat(_playerType, _statInfo);
+}
+
 // 세션 참조 해제
 void Player::DetachSession()
 {
 	_session.reset();
 	_session = nullptr;
-}
-
-// 맵 이동
-void Player::MoveMap(MapId targetMapId)
-{
-	_currentMap = targetMapId;
 }
 
 // 세션 가져오기
@@ -36,24 +36,12 @@ SessionRef Player::GetSession()
 	return _session;
 }
 
-// 현재 있는 맵
-MapId Player::CurrentMap() const
-{
-	return _currentMap;
-}
-
-// 포지션 설정
-void Player::SetPosition(const Protocol::PosInfo& posInfo)
-{
-	_posInfo = posInfo;
-}
-
 // 플레이어 정보(Protocol::PlayerInfo) 만들기
-Protocol::PlayerInfo Player::MakePlayerInfo()
+void Player::MakePlayerInfo(Protocol::PlayerInfo& outPlayerInfo)
 {
-	Protocol::PlayerInfo playerInfo;
-	*playerInfo.mutable_info() = MakeObjectInfo();
-	playerInfo.set_playertype(_playerType);
+	Protocol::ObjectInfo objectInfo;
+	MakeObjectInfo(objectInfo);
 
-	return playerInfo;
+	outPlayerInfo.mutable_info()->CopyFrom(objectInfo);
+	outPlayerInfo.set_playertype(_playerType);
 }
--- GameServer/Object/Player/Player.h
+++ GameServer/Object/Player/Player.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Object/GameObject.h"
-#include "Zone/MapDefine.h"
 
 /*-------------------------------------------------------
 				Player
@@ -12,25 +11,17 @@ public:
 	Player(SessionRef session, const Protocol::PlayerType& playerType, const std::string& name);
 	virtual ~Player();
 
+	// 초기 스텟 정보 초기화
+	void InitStatInfo();
 	// 세션 참조 해제
 	void DetachSession();
-	// 맵 이동
-	void MoveMap(MapId targetMapId);
 	// 세션 가져오기
 	SessionRef GetSession();
-	// 현재 있는 맵 가져오기
-	MapId CurrentMap() const;
-	// 포지션 설정
-	void SetPosition(const Protocol::PosInfo& posInfo);
-
 	// 플레이어 정보(Protocol::PlayerInfo) 만들기
-	Protocol::PlayerInfo MakePlayerInfo();
+	void MakePlayerInfo(Protocol::PlayerInfo& outPlayerInfo);
 
 private:
 	// 캐릭터 정보
 	SessionRef _session;
 	Protocol::PlayerType _playerType;
-
-	// 캐릭터 상태
-	MapId _currentMap;
 };
