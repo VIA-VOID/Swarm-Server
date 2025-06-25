36c680205dc4d06c15fbdd8fc455ce1b577a2b6c
jeondoh <jundo1302@gmail.com>
2025-06-25 23:29:47 +0900

    fix: 위치 관련 함수 추가

--- GameServer/Object/GameObject.cpp
+++ GameServer/Object/GameObject.cpp
@@ -1,10 +1,9 @@
 #include "pch.h"
 #include "GameObject.h"
-#include "ObjectDefine.h"
 
 GameObject::GameObject()
 	: _objectId(ObjectId::Generate()), _objectType(Protocol::ObjectType::OBJECT_TYPE_NONE),
-	_objectState(Protocol::ObjectState::OBJECT_STATE_NONE)
+	_objectState(Protocol::ObjectState::OBJECT_STATE_NONE), _vectorPos()
 {
 }
 
@@ -13,21 +12,59 @@ GameObject::~GameObject()
 }
 
 // ObjectId 가져오기
-ObjectId GameObject::GetObjectId()
+ObjectId GameObject::GetObjectId() const
 {
 	return _objectId;
 }
 
-// Object 공용 정보(Protocol::ObjectInfo) 만들기
-Protocol::ObjectInfo GameObject::MakeObjectInfo()
+// 위치 가져오기(Vector3d)
+void GameObject::GetWorldPosition(Vector3d& outVector3d) const
 {
-	Protocol::ObjectInfo objectInfo;
-	objectInfo.set_objectid(_objectId.GetId());
-	objectInfo.set_type(_objectType);
-	*objectInfo.mutable_posinfo() = _posInfo;
-	objectInfo.set_state(_objectState);
-	*objectInfo.mutable_statinfo() = _statInfo;
-	objectInfo.set_name(_name);
+	outVector3d = _vectorPos;
+}
 
-	return objectInfo;
+// 위치 가져오기(Protocol::PosInfo)
+void GameObject::GetWorldPosition(Protocol::PosInfo& outPosInfo) const
+{
+	outPosInfo.CopyFrom(_pos);
+}
+
+// 위치 업데이트
+void GameObject::SetWorldPosition(const Protocol::PosInfo& posInfo)
+{
+	_pos.CopyFrom(posInfo);
+	_vectorPos.SetWorldX(posInfo.x());
+	_vectorPos.SetWorldY(posInfo.y());
+}
+
+// 위치 업데이트
+void GameObject::SetWorldPosition(const Vector3d& vectorPos)
+{
+	_vectorPos.SetWorldX(vectorPos.GetWorldX());
+	_vectorPos.SetWorldY(vectorPos.GetWorldY());
+	_pos.set_x(vectorPos.GetWorldX());
+	_pos.set_x(vectorPos.GetWorldY());
+}
+
+// Player인지 확인
+bool GameObject::IsPlayer() const
+{
+	return _objectType == Protocol::ObjectType::OBJECT_TYPE_PLAYER;
+}
+
+// Monster인지 확인
+bool GameObject::IsMonster() const
+{
+	return _objectType == Protocol::ObjectType::OBJECT_TYPE_MONSTER;
+}
+
+// Object 공용 정보(Protocol::ObjectInfo) 만들기
+void GameObject::MakeObjectInfo(Protocol::ObjectInfo& outObjectInfo)
+{
+	outObjectInfo.set_objectid(_objectId.GetId());
+	outObjectInfo.set_type(_objectType);
+	outObjectInfo.mutable_posinfo()->CopyFrom(_pos);
+	outObjectInfo.set_state(_objectState);
+	outObjectInfo.mutable_statinfo()->CopyFrom(_statInfo);
+	outObjectInfo.set_name(_name);
 }
--- GameServer/Object/GameObject.h
+++ GameServer/Object/GameObject.h
@@ -1,6 +1,5 @@
 #pragma once
-
-class ObjectId;
+#include "ObjectDefine.h"
 
 /*-------------------------------------------------------
 				GameObject
@@ -13,12 +12,23 @@ class GameObject : public std::enable_shared_from_this<GameObject>
 public:
 	GameObject();
 	virtual ~GameObject();
+
 	// ObjectId 가져오기
-	ObjectId GetObjectId();
+	ObjectId GetObjectId() const;
+	// 위치 가져오기
+	void GetWorldPosition(Vector3d& outVector3d) const;
+	void GetWorldPosition(Protocol::PosInfo& outPosInfo) const;
+	// 위치 업데이트
+	void SetWorldPosition(const Protocol::PosInfo& posInfo);
+	void SetWorldPosition(const Vector3d& vectorPos);
+	// Player인지 확인
+	bool IsPlayer() const;
+	// Monster인지 확인
+	bool IsMonster() const;
 
 protected:
 	// Object 공용 정보(Protocol::ObjectInfo) 만들기
-	Protocol::ObjectInfo MakeObjectInfo();
+	void MakeObjectInfo(Protocol::ObjectInfo& outObjectInfo);
 
 protected:
 	// 고유 ID
@@ -26,12 +36,12 @@ protected:
 	// Object 타입
 	Protocol::ObjectType _objectType;
 	// 위치 및 방향 정보
-	Protocol::PosInfo _posInfo;
+	Protocol::PosInfo _pos;
+	Vector3d _vectorPos;
 	// Object 상태
 	Protocol::ObjectState _objectState;
 	// 스탯 정보
 	Protocol::StatInfo _statInfo;
 	// 이름
 	std::string _name;
-
 };
