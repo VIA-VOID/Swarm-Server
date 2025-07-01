8086743879de03903b20ff17a71e887a7fb32d06
jeondoh <jundo1302@gmail.com>
2025-07-01 23:48:18 +0900

    fix: 캐릭터 시야 범위 감소

--- GameServer/GameDefine.h
+++ GameServer/GameDefine.h
@@ -4,5 +4,5 @@
 constexpr std::chrono::milliseconds FRAME_INTERVAL = std::chrono::milliseconds(50);
 // 좌표 보정 수치(기본 소수점 3자리)
 constexpr int32 POS_REVISE_NUM = 1000;
-// Player 시야 범위
-constexpr uint16 PLAYER_VISUAL_RANGE = 100;
+// Player 시야 범위(Grid 기준)
+constexpr uint16 PLAYER_VISUAL_RANGE = 80;
