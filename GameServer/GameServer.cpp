a3374874b54da90f0cdf2b0ecf2fad3be6eaca7c
jeondoh <jundo1302@gmail.com>
2025-07-06 01:00:58 +0900

    feat: 게임 종료시 주변 캐릭터에 패킷 전송 함수 추가

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -42,11 +42,21 @@ void GameServer::OnDisconnected(SessionRef session)
 		return;
 	}
 
-	// 상호 참조 해제 전 despawn 패킷 전송
-	player->LeaveGame();
+	// 해제전 필요 정보 복사
+	ObjectId objectId = player->GetObjectId();
+	ZoneType zoneType = player->GetCurrentZone();
+	Vector3d position = player->GetWorldPosition();
+	GridIndex gridIndex = player->GetCurrentGrid();
+
 	// 상호 참조 해제
 	player->DetachSession();
 	session->DetachPlayer();
+
+	// 유효한 세션을 가진 주변 플레이어들에게 알림
+	JobQ.DoAsync([this, objectId, zoneType, position, gridIndex]()
+		{
+			LeaveGame(objectId, zoneType, position, gridIndex);
+		}, JobGroups::Network);
 }
 
 void GameServer::OnRecv(SessionRef session, BYTE* buffer, int32 len)
@@ -57,4 +67,29 @@ void GameServer::OnRecv(SessionRef session, BYTE* buffer, int32 len)
 void GameServer::OnSend(SessionRef session, int32 len)
 {
 
+}
+
+// 게임 종료시 주변 플레이어에게 알림
+void GameServer::LeaveGame(const ObjectId objectId, const ZoneType zoneType, const Vector3d& position, const GridIndex& gridIndex)
+{
+	// 그리드에서 제거
+	WorldMgr.RemoveObjectToZone(objectId, zoneType, gridIndex);
+
+	// 디스폰 패킷 생성
+	Protocol::SC_PLAYER_DESPAWN despawnPkt;
+	despawnPkt.set_objectid(objectId.GetId());
+
+	// 시야 내 근처 플레이어에게 전송
+	Vector<PlayerRef> players;
+	WorldMgr.GetVisiblePlayersInZones(position, players);
+	
+	for (const auto& player : players)
+	{
+		SessionRef playerSession = player->GetSession();
+		if (playerSession)
+		{
+			std::cout << "너 여기 들어와?\n";
+			PacketHandler::SendPacket(playerSession, despawnPkt, PacketID::SC_PLAYER_DESPAWN);
+		}
+	}
 }
--- GameServer/GameServer.h
+++ GameServer/GameServer.h
@@ -15,4 +15,8 @@ public:
 	void OnDisconnected(SessionRef session) override;
 	void OnRecv(SessionRef session, BYTE* buffer, int32 len) override;
 	void OnSend(SessionRef session, int32 len) override;
+
+private:
+	// 게임 종료시 주변 플레이어에게 알림
+	void LeaveGame(const ObjectId objectId, const ZoneType zoneType, const Vector3d& position, const GridIndex& gridIndex);
 };
