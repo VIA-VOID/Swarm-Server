381c405bf1b968398b02b5af01d580e8af7d886e
jeondoh <jundo1302@gmail.com>
2025-07-17 05:40:58 +0900

    feat: 상수 추가

--- GameServer/GameDefine.h
+++ GameServer/GameDefine.h
@@ -1,6 +1,9 @@
 #pragma once
 
-// 좌표 보정 수치(기본 소수점 3자리)
-constexpr int32 POS_REVISE_NUM = 1000;
 // Player 시야 범위(Grid 기준)
-constexpr uint16 PLAYER_VISUAL_RANGE = 80;
+constexpr uint16 PLAYER_VISUAL_RANGE = 40;
+// 섹터 크기 ex) 8*8 그리드 = 1개 섹터
+constexpr uint16 SECTOR_SIZE = 8;
+// 캐릭터 이동 허용 오차(%)
+constexpr float MOVE_ALLOW_RANGE = 1.2f;
+
