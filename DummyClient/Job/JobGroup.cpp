784b7705c4d682bd314816d063192d5c83f37180
jeondoh <jundo1302@gmail.com>
2025-07-21 23:20:00 +0900

    fix: JobGroup 등록 간소화

--- DummyClient/Job/JobGroup.cpp
+++ DummyClient/Job/JobGroup.cpp
@@ -1,22 +1,21 @@
 #include "pch.h"
 #include "JobGroup.h"
 
-#include <typeindex>
-
-#define JOB_GROUP(name, priority) \
-    name = JobGroupMgr.RegisterContentGroup(#name, priority);
-
-#define CLASS_TO_JOB(className, groupName) \
-    JobGroupMgr.SetTypeToGroup(std::type_index(typeid(className)), groupName); \
-    JobQ.RegisterThreadsForGroup(groupName);
+#define JOB_GROUP(name) \
+    name = JobGroupMgr.RegisterContentGroup(#name); \
+	JobQ.MakeGroupThread(JobGroup::name);
 
 /*----------------------------
 		JobGroup
 ----------------------------*/
 
 // 정적 멤버 정의
+JobGroupId JobGroup::Social = JobGroups::Invalid;
+JobGroupId JobGroup::Timer = JobGroups::Invalid;
 
 // JobGroup 등록
 void JobGroup::Init()
 {
+	JOB_GROUP(Social);
+	JOB_GROUP(Timer);
 }
