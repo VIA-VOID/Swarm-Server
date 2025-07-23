f5bf53ac2029b4dcdf357f2fa896afb179c5ec9e
jeondoh <jundo1302@gmail.com>
2025-07-23 23:10:18 +0900

    fix: 섹터 크기 변경

--- GameServer/GameDefine.h
+++ GameServer/GameDefine.h
@@ -3,6 +3,6 @@
 // Player 시야 범위(Grid 기준)
 constexpr uint16 PLAYER_VISUAL_RANGE = 40;
 // 섹터 크기 ex) 10*10 그리드 = 1개 섹터
-constexpr uint16 SECTOR_SIZE = 10;
+constexpr uint16 SECTOR_SIZE = 20;
 // 캐릭터 이동 허용 오차(%)
 constexpr float MOVE_ALLOW_RANGE = 1.2f;
