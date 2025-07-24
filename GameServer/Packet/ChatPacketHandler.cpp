ddaac9993349b50b6fe8d28249dbc0f134ed8963
jeondoh <jundo1302@gmail.com>
2025-07-24 23:51:34 +0900

    feat: 채팅 패킷 전달

--- GameServer/Packet/ChatPacketHandler.cpp
+++ GameServer/Packet/ChatPacketHandler.cpp
@@ -1,7 +1,14 @@
 #include "pch.h"
 #include "ChatPacketHandler.h"
+#include "Object/Player/Player.h"
 
+// 채팅 패킷
 void ChatPacketHandler::Handle_CS_CHAT_MSG(SessionRef session, const Protocol::CS_CHAT_MSG& packet)
 {
-
+	Player* player = session->GetPlayer<Player>();
+	if (player == nullptr || player->IsValid() == false)
+	{
+		return;
+	}
+	player->ChatMessage(packet);
 }
